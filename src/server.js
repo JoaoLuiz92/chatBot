9dc684b00677d9d60fa7dc4c3666d162dfa9b8ef
JoaoLuiz92 <jluizdarosajr@gmail.com>
2024-03-12 23:59:32 -0300

    ajustando chatbot

--- src/server.js
+++ src/server.js
@@ -217,24 +217,63 @@ const swaggerDocument = {
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+const thanksResponses = [
+    "De nada! Se precisar de mais alguma coisa, estou à disposição.",
+    "Por nada! Estou aqui para tornar sua experiência o mais agradável possível.",
+    "Fico feliz em poder ajudar! Se tiver mais alguma pergunta, é só me chamar."
+];
+const greetingsResponses = [
+    "Olá! Como posso ajudar?",
+    "Oi! O que você gostaria de saber?",
+    "Olá! Estou aqui para ajudar. Qual é a sua dúvida?"
+];
+
+const cityGuideResponses = [
+    "Aqui estão algumas atrações populares em Paris...",
+    "Você não pode perder a Torre Eiffel e o Louvre em Paris!",
+    "Paris é famosa por sua arquitetura deslumbrante e museus incríveis. Aqui estão algumas recomendações..."
+];
+
+
+
 app.post('/', async (req, res) => {
-    const userMessage = req.body;
+    const userMessage = req.body.message;
+    
 
         const requestBody = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-          {
-            "role": "system",
-            "content": "You are a helpful assistant."
-          },
-          {
-            "role": "user",
-            "content": "Hello!"
-          }
-        ]
-    };
-  
-    try {
+       
+        "input_data": {
+            "input_string": [
+            {
+                "role": "user",
+                "content": "I am going to Paris, what should I see?"
+            },
+            {
+                "role": "assistant",
+                "content": "Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world."
+            },
+            {
+                "role": "user",
+                "content": "What is so great about #1?"
+            },
+
+            {
+                "role" : "",
+                "content" : ""
+            }
+            ],
+            
+            "parameters": {
+            "temperature": 0.6,
+            "top_p": 0.9,
+            "do_sample": true,
+            "max_new_tokens": 200
+            }
+        }
+}
+
+
+try {
         // Fazer a chamada ao serviço Llama2 usando os dados da requisição e as variáveis de ambiente
         const llamaResponse = await axios.post(process.env.LLAMA_ENDPOINT, requestBody, {
             headers: {
@@ -245,8 +284,24 @@ app.post('/', async (req, res) => {
          
 });
 
+        // Extrair a resposta do LLAMA
+        const llamaData = llamaResponse.data;
+
+        // Escolher uma resposta de agradecimento aleatoriamente
+        const randomThanksResponse = thanksResponses[Math.floor(Math.random() * thanksResponses.length)];
+        const randomgreetingsResponses = greetingsResponses[Math.floor(Math.random() * greetingsResponses.length)];
+        const randomcityGuideResponses = cityGuideResponses[Math.floor(Math.random() * cityGuideResponses.length)];
+
+
+
+
 // Enviar a resposta do Llama2 de volta ao cliente
-res.status(200).json(llamaResponse.data);
+res.status(200).json({llamaResponse: llamaData,
+    thanksResponses: randomThanksResponse,
+    greetingsResponses:randomgreetingsResponses,
+    cityGuideResponses:randomcityGuideResponses
+}
+    );
 } catch (error) {
     console.error('Erro na chamada do serviço Llama2:', error.message);
       res.status(500).json({ message: 'Erro no servidor' });
@@ -259,131 +314,6 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-
-/*app.post('/api/chat', async (req, res) => {
-app.get('/health', (req, res) => {
-  res.status(200).json({ healthy: '' });
-});
-
-app.post('/v1/chat/completion', async (req, res) => {
-  const requestBody = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-    {
-      "role": "system",
-      "content": "You are a helpful assistant."
-    },
-    {
-      "role": "user",
-      "content": "Hello!"
-    },
-
-    {
-      "model": "gpt-3.5-turbo-instruct",
-      "prompt": "Say this is a test",
-      "max_tokens": 7,
-      "temperature": 0
-    },
-
-    {
-      "input_data": {
-        "input_string": [
-          {
-            "role": "user",
-            "content": "I am going to Paris, what should I see?"
-          },
-          {
-            "role": "assistant",
-            "content": "Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world."
-          },
-          {
-            "role": "user",
-            "content": "What is so great about #1?"
-          }
-        ],
-        "parameters": {
-          "temperature": 0.6,
-          "top_p": 0.9,
-          "do_sample": true,
-          "max_new_tokens": 200
-        }
-      }
-    }
-  ]
-  }
-  
- 
-
-  const llamaResponse = await axios.post(process.env.LLAMA_ENDPOINT,requestBody,{
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.LLAMA_KEY}`
-    },
-
-  });
-  try {
-    const processedResponse = llamaResponse.data;
-    
-
-  res.status(200).json(
-    (processedResponse),
-    {
-    id: 'chatcmpl-123',
-    object: 'chat.completion',
-    created: 1677652288,
-    model: 'gpt-3.5-turbo-0613',
-    choices: [
-      {
-        index: 0,
-        message: {
-          role: 'assistant',
-          content: '\n\nHello there, how may I assist you today?',
-        },
-        finish_reason: 'stop',
-      },
-    ],
-    usage: {
-      prompt_tokens: 9,
-      completion_tokens: 12,
-      total_tokens: 21,
-    },
-
-    });
-  
-  } catch (error) {
-    console.error('Erro na chamada do Llama2:', error.message);
-    res.status(500).json({ message: 'Erro no servidor' });
-  }// Lógica para processar a resposta do Llama2
-  
-});
-
-
-
-  const requestBody = req.body.message;
-
-  try {
-    const llamaResponse = await axios.post(LLAMA_ENDPOINT, requestBody,{
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${LLAMA_KEY}`
-      },
-
-    });
-
-      const processedResponse = processLlamaResponse(llamaResponse.data);
-
-    res.json({ output: processedResponse });
-  } catch (error) {
-    console.error('Erro na chamada do Llama2:', error.message);
-    res.status(500).json({ message: 'Erro no servidor' });
-  }
-});
-
-// Função para processar a resposta do Llama2
-function processLlamaResponse(llamaResponse) {
-  // Adicione sua lógica aqui conforme necessário
-  return llamaResponse.output;
-}*/
 
 
 
